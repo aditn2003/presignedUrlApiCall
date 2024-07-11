d19f3242fbaba1f0f17986beb14304d00860888a
aditn2003 <an238@njit.edu>
2024-07-12 00:06:22 +0530

    added swagger UI dependedcies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ repositories {
 }
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("software.amazon.awssdk:s3:2.20.48")
@@ -37,6 +38,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    
 }
 
 kotlin {
